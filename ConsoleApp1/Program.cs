708a7e8c6dfa33cea9de15e067241fb0d4a18541
Neil45623 <86229955+Neil45623@users.noreply.github.com>
2023-01-02 12:02:34 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+enum JourSemaine
+{
+    Lundi,
+    Mardi,
+    Mercredi,
+    Jeudi,
+    Vendredi,
+    Samedi,
+    Dimanche
+}
+class Program
+{
+    public static void Main()
+    {
+        JourSemaine Aujourdhui = JourSemaine.Lundi;
+        JourSemaine AujourdhuiB = 0;
+
+        Console.WriteLine(Aujourdhui);
+        Console.WriteLine(AujourdhuiB);
+    }
+}
