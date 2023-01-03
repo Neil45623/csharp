e2ceaf65c812184e76d4fc6f8c2342e488695b3f
Neil45623 <86229955+Neil45623@users.noreply.github.com>
2023-01-03 15:11:10 +0100

    push studies

---
+++ TP2/Formation.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+
+namespace Formation
+{
+    class Etudiant
+    {
+        public string Nom { get; set; }
+        public int Age { get; set; }
+    }
+
+    class Formation
+    {
+        public List<Etudiant> Etudiants { get; set; }
+
+        public Formation()
+        {
+            Etudiants = new List<Etudiant>();
+        }
+
+        public double CalculerMoyenneAge()
+        {
+            int totalAge = 0;
+            foreach (Etudiant etudiant in Etudiants)
+            {
+                totalAge += etudiant.Age;
+            }
+            return (double)totalAge / Etudiants.Count;
+        }
+    }
+}
