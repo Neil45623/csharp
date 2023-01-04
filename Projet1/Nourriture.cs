8b920746ac6536741f339ba52e947e4716d2d091
Neil45623 <86229955+Neil45623@users.noreply.github.com>
2023-01-04 12:09:09 +0100

    nourriture

--- Projet1/Nourriture.cs
+++ Projet1/Nourriture.cs
@@ -8,5 +8,22 @@ namespace Projet1
 {
     public class Nourriture
     {
+        public Position nourriturePos = new Position();
+        Random rdm = new Random();
+        Canvas canvas = new Canvas();
+
+
+        public Nourriture()
+        {
+            nourriturePos.x = rdm.Next(5, canvas.Widht);
+            nourriturePos.y = rdm.Next(5, canvas.Height);
+        }
+
+        public void drawNourriture()
+        {
+            Console.SetCursorPosition(nourriturePos.x, nourriturePos.y);
+            Console.Write("");
+        }
+
     }
 }
--- Projet1/Position.cs
+++ Projet1/Position.cs
@@ -10,6 +10,10 @@ namespace Projet1
     {
         public int x { get; set; }
         public int y { get; set; }
+        public Position()
+        {
+
+        }
         public Position(int x, int y)
         {
             this.x = x;
